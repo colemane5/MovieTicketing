225691958cdbe0007dfbf4c94d06144b655dc4aa
Ethan Coleman <colemane@ksu.edu>
2024-04-23 10:06:19 -0500

    Drafted showtime selection view/viewmodel.

--- MovieTicketingClient/ViewModels/ShowtimeSelectionViewModel.cs
+++ MovieTicketingClient/ViewModels/ShowtimeSelectionViewModel.cs
@@ -5,12 +5,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SharedResources.Models;
 
 namespace MovieTicketingClient.ViewModels
 {
     public class ShowtimeSelectionViewModel : ViewModelBase
     {
         //to use for data binding once viewmodel is developed
-        //private readonly SqlTheaterRepository theaterRepository = new();
+        private readonly SqlTheaterRepository theaterRepository = new();
+
+        public Movie SelectedMovie { get; }
+
+        public string MovieTitle => SelectedMovie.Title;
+
+        public List<Theater> AvailableTheaters { get; }
+
+        private Theater _selectedTheater;
+        public Theater SelectedTheater
+        {
+            get => _selectedTheater;
+            set
+            {
+                _selectedTheater = value;
+                OnPropertyChanged(nameof(SelectedTheater));
+            }
+        }
+
+        private List<MovieShowtime> _availableShowtimes;
+        public List<MovieShowtime> AvailableShowtimes
+        {
+            get => _availableShowtimes;
+            set
+            {
+                _availableShowtimes = value;
+                OnPropertyChanged(nameof(AvailableShowtimes));
+            }
+        }
+
+        private MovieShowtime _selectedShowtime;
+        public MovieShowtime SelectedShowtime
+        {
+            get => _selectedShowtime;
+            set
+            {
+                _selectedShowtime = value;
+                OnPropertyChanged(nameof(SelectedShowtime));
+            }
+        }
+
+        public ICommand ReserveShowtimeCommand { get; }
     }
 }
