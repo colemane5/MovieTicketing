c35f1006f4b44df9d25e6fcf48ba5b3ad4db4e86
jakeesmith01 <111781011+jakeesmith01@users.noreply.github.com>
2024-04-23 16:56:51 -0500

    Update GetTicket.sql

--- MovieDBSqlData/SQLProcedures/GetTicket.sql
+++ MovieDBSqlData/SQLProcedures/GetTicket.sql
@@ -3,7 +3,7 @@ CREATE OR ALTER PROCEDURE GetTicket
 	@MovieShowtimeID INT,
 	@SalePrice DECIMAL(10, 2),
 	@SeatsLeft INT,
-	@Result NVARCHAR(10) OUT
+	@Result BIT OUT
 AS
 BEGIN
 	SET NOCOUNT ON
@@ -17,13 +17,20 @@ BEGIN
 	BEGIN
 		INSERT INTO MovieDB.TicketPurchase(UserID, MovieShowtimeID, SalePrice)
 		VALUES(@UserID, @MovieShowtimeID, @SalePrice)
+
 		UPDATE MovieDB.MovieShowtime
-		SET SeatsAvailable = @SeatsLeft
-		WHERE MovieShowtimeID = @MovieShowtimeID
-		SET @Result = N'Success'
+		SET SeatsLeft = (
+			SELECT T.SeatsAvailable - COUNT(DISTINCT TP.MovieShowtimeID)
+			FROM MovieDB.Theater T
+			LEFT JOIN MovieDB.MovieShowtime M ON T.TheaterID = M.TheaterID
+			LEFT JOIN MovieDB.TicketPurchase TP ON M.MovieShowtimeID = TP.MovieShowtimeID
+			WHERE M.MovieShowtimeID = @MovieShowtimeID
+		);
+
+		SET @Result = 1;
 	END
 	ELSE
 	BEGIN
-		SET @Result = N'Failed'
+		SET @Result = 0
 	END
 END
