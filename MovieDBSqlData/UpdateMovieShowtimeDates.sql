7e00059656c77731d5e79937c1911534bac747f8
jakeesmith01 <111781011+jakeesmith01@users.noreply.github.com>
2024-04-23 13:24:52 -0500

    Add files via upload

--- MovieDBSqlData/UpdateMovieShowtimeDates.sql
+++ MovieDBSqlData/UpdateMovieShowtimeDates.sql
@@ -1,8 +1,8 @@
 
 --Changes every rows month portion of their StartOn, 4 to 1, 5 to 2, etc.
 UPDATE MovieDB.MovieShowtime
-SET StartOn = DATEFROMPARTS(
-	YEAR(StartOn), CASE WHEN DATEPART(MONTH, StartOn) = 4 THEN 1
+SET StartOn = DATEADD(MONTH, 
+					CASE WHEN DATEPART(MONTH, StartOn) = 4 THEN 1
 						 WHEN DATEPART(MONTH, StartOn) = 5 THEN 2
 						 WHEN DATEPART(MONTH, StartOn) = 6 THEN 3
 						 WHEN DATEPART(MONTH, StartOn) = 7 THEN 4
@@ -15,14 +15,16 @@ SET StartOn = DATEFROMPARTS(
 UPDATE MovieDB.MovieShowtime
 SET StartOn = DATEADD(DAY, ABS(CHECKSUM(NEWID())) % 28, StartOn)
 
-
---Changes every rows time portion of their StartOn to a value between 17 and 21
+--Changes every rows time portion of their StartOn to a value between 17 and 21 if its not already
 UPDATE MovieDB.MovieShowtime
-SET StartOn = DATEADD(HOUR, 17 + ABS(CHECKSUM(NEWID())) % 5, StartOn)
+SET StartOn = DATEADD(HOUR, CAST(RAND() * (21 - 17 + 1) + 17 AS INT), StartOn)
+WHERE DATEPART(HOUR, StartOn) NOT BETWEEN 17 AND 21
 
 --Changes MovieShowtimeID 1 to StartOn January 1, 2024
 UPDATE MovieDB.MovieShowtime
 SET StartOn = DATEADD(DAY, 1 - DAY(StartOn), StartOn)
 WHERE MovieShowtimeID = 1
 
+--checking to see if the random values were random enough (didnt create duplicate keys)
 SELECT * FROM MovieDB.MovieShowtime
+WHERE DATEPART(HOUR, StartOn) NOT BETWEEN 17 AND 21
