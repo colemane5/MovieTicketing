4f2d2df05f2d713d081e46ccae4a6b8f40148040
jakeesmith01 <111781011+jakeesmith01@users.noreply.github.com>
2024-04-23 14:33:36 -0500

    Add files via upload

--- MovieDBSqlData/AggQueries/MovieStatisticsOverGivenPeriod.sql
+++ MovieDBSqlData/AggQueries/MovieStatisticsOverGivenPeriod.sql
@@ -5,8 +5,8 @@ AS
 BEGIN
 	SELECT M.MovieTitle,
 	       COUNT(T.UserID) AS TicketSales,
-		   COUNT(MS.MovieID) AS TotalShowings,
-	       IIF(COUNT(MS.MovieID) > 0, CAST(COUNT(T.UserID) AS FLOAT) / COUNT(MS.MovieID), 0.00) AS AvgTicketsPerShowing
+		   COUNT(DISTINCT MS.MovieShowtimeID) AS TotalShowings,
+	       IIF(COUNT(DISTINCT MS.MovieShowtimeID) > 0, ROUND(CAST(COUNT(DISTINCT T.UserID) AS FLOAT) / COUNT(DISTINCT MS.MovieShowtimeID), 0), 0) AS AvgTicketsPerShowing
 	FROM MovieDB.Movie M
 	LEFT JOIN MovieDB.MovieShowtime MS ON M.MovieID = MS.MovieID
 		AND MS.StartOn BETWEEN @StartTime AND @EndTime
